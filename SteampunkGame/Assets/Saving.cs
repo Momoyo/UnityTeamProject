969386ae2a5bac599e6368559dc2f7fa6f66c2d8
BK-TN <me@bk-tn.com>
2014-12-12 15:23:52 +0100

    i have to commit my changes apparently

---
+++ SteampunkGame/Assets/Saving.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Saving {
+	public static List<SavedBehaviour> savedScripts = new List<SavedBehaviour>();
+	public static void ListSavedFields() {
+		System.Attribute attr = System.Attribute.GetCustomAttribute(typeof(TestSavedScript),typeof(SavedAttribute));
+		Debug.Log (attr.ToString());
+
+	}
+}
+
+public class SavedBehaviour : MonoBehaviour {
+	public SavedBehaviour() {
+
+	}
+}
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class SavedAttribute : System.Attribute {
+	public SavedAttribute() {
+		Debug.Log("test");
+	}
+}
---
+++ SteampunkGame/Assets/TestSavedScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSavedScript : MonoBehaviour {
+
+	[Saved]
+	public string text = "I am a floor!!";
+}
